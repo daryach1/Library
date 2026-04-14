0385871b4a5333c34560570c832fe65f0944c924
Daria <chercasova_dasha@mail.ru>
2026-04-14 16:57:59 +0700

    Create application

---
+++ LibraryDesktop/Windows/MainLibrary.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+
+namespace LibraryDesktop.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainLibrary : Window
+    {
+        public MainLibrary()
+        {
+            InitializeComponent();
+        }
+    }
+}
