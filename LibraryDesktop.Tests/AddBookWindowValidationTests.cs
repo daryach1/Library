ba40bc2a0012af7c98c1b4005bc7f792fe95ba92
Rustakars <rustam.karabarin2004@gmail.com>
2026-06-08 15:42:10 +0700

    Реализованы некоторые тесты

---
+++ LibraryDesktop.Tests/AddBookWindowValidationTests.cs
@@ -0,0 +1,81 @@
+﻿using LibraryDesktop.Classes;
+using LibraryDesktop.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+using System.Windows.Controls;
+
+namespace LibraryDesktop.Tests
+{
+    [TestClass]
+    public class AddBookWindowValidationTests
+    {
+        private void SetPrivateField(object obj, string fieldName, object value)
+        {
+            var field = obj.GetType().GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field != null)
+            {
+                field.SetValue(obj, value);
+            }
+            else
+            {
+                Assert.Fail($"Поле {fieldName} не найдено в классе {obj.GetType().Name}");
+            }
+        }
+
+        private object InvokePrivateMethod(object obj, string methodName)
+        {
+            var method = obj.GetType().GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Instance);
+            return method?.Invoke(obj, null);
+        }
+
+        [TestMethod]
+        [STAThread]
+        public void ValidateAllFields_WithValidData_ReturnsTrue()
+        {
+            var window = new AddBookWindow();
+            SetPrivateField(window, "TitleTextBox", new TextBox { Text = "Название книги" });
+            SetPrivateField(window, "AuthorTextBox", new TextBox { Text = "Автор" });
+            SetPrivateField(window, "GenreTextBox", new TextBox { Text = "Жанр" });
+            SetPrivateField(window, "TotalCopiesTextBox", new TextBox { Text = "10" });
+            SetPrivateField(window, "AvailableCopiesTextBox", new TextBox { Text = "5" });
+
+            bool isValid = (bool)InvokePrivateMethod(window, "ValidateAllFields");
+
+            Assert.IsTrue(isValid, "Валидация должна пройти успешно при корректных данных");
+        }
+
+        [TestMethod]
+        [STAThread]
+        public void ValidateAllFields_WithAvailableGreaterThanTotal_ReturnsFalse()
+        {
+            var window = new AddBookWindow();
+            SetPrivateField(window, "TitleTextBox", new TextBox { Text = "Название" });
+            SetPrivateField(window, "AuthorTextBox", new TextBox { Text = "Автор" });
+            SetPrivateField(window, "GenreTextBox", new TextBox { Text = "Жанр" });
+            SetPrivateField(window, "TotalCopiesTextBox", new TextBox { Text = "5" });
+            SetPrivateField(window, "AvailableCopiesTextBox", new TextBox { Text = "10" });
+
+            bool isValid = (bool)InvokePrivateMethod(window, "ValidateAllFields");
+
+            Assert.IsFalse(isValid, "Валидация должна вернуть false, если доступных книг больше, чем общих");
+        }
+
+        [TestMethod]
+        [STAThread]
+        public void ValidateAllFields_WithNegativeNumbers_ReturnsFalse()
+        {
+            var window = new AddBookWindow();
+            SetPrivateField(window, "TitleTextBox", new TextBox { Text = "Название" });
+            SetPrivateField(window, "AuthorTextBox", new TextBox { Text = "Автор" });
+            SetPrivateField(window, "GenreTextBox", new TextBox { Text = "Жанр" });
+            SetPrivateField(window, "TotalCopiesTextBox", new TextBox { Text = "-5" }); // Отрицательное число
+            SetPrivateField(window, "AvailableCopiesTextBox", new TextBox { Text = "2" });
+
+            bool isValid = (bool)InvokePrivateMethod(window, "ValidateAllFields");
+
+            Assert.IsFalse(isValid, "Валидация должна вернуть false при отрицательных значениях");
+        }
+
+    }
+}
---
+++ LibraryDesktop.Tests/BookTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryDesktop.Classes;
+
+namespace LibraryDesktop.Tests
+{
+    [TestClass]
+    public class BookTests
+    {
+        [TestMethod]
+        public void Book_Properties_ShouldBeSetAndRerievedCorrectly()
+        {
+            var book = new Book
+            {
+                Title = "Война и мир",
+                Author = "Лев Толстой",
+                Genre = "Роман",
+                TotalCopies = 10,
+                AvailableCopies = 7
+            };
+
+            Assert.AreEqual("Война и мир", book.Title);
+            Assert.AreEqual("Лев Толстой", book.Author);
+            Assert.AreEqual("Роман", book.Genre);
+            Assert.AreEqual(10, book.TotalCopies);
+            Assert.AreEqual(7, book.AvailableCopies);
+        }
+
+        [TestMethod]
+        public void Book_DefaultValues_ShouldBeValid()
+        {
+            var book = new Book();
+
+            Assert.IsNull(book.Title);
+            Assert.AreEqual(0, book.TotalCopies);
+            Assert.AreEqual(0, book.AvailableCopies);
+        }
+
+
+    }
+}
+
